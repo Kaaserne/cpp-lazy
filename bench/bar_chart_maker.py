d271e039e0804b71340e7a4309f1eb77c0cda690
MarcDirven <github@mdy.mozmail.com>
2025-01-17 16:40:17 +0100

    Remove cmake parts

--- bench/bar_chart_maker.py
+++ bench/bar_chart_maker.py
@@ -74,41 +74,8 @@ def make_table(benchmark_records: Iterable, cxx_version: str, n_iterations: int)
     canvas.print_figure(f'benchmarks-iterators-{cxx_version}.png',
                         bbox_inches='tight', dpi=200)
 
-async def run_command(command: str):
-    process = await asyncio.create_subprocess_shell(command, shell=True,
-        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    async for line in process.stdout:
-        print(line.decode().strip())
-    await process.wait()
-    if process.returncode != 0:
-        async for line in process.stderr:
-            print(line.decode().strip())
-        return False
-    return True
-
-async def configure_cmake(cxx_version: str, path: str, compiler_path: str):
-    if os.path.exists(os.path.join(path, 'CMakeCache.txt')):
-        return
-    cmd = f'cmake -DCMAKE_CXX_STANDARD={cxx_version} -DCMAKE_BUILD_TYPE:STRING=Relase -DCMAKE_C_COMPILER:FILEPATH="{compiler_path}" "-DCMAKE_CXX_COMPILER:FILEPATH={compiler_path}" -S {os.path.join(path, "..")} -B {path} -G Ninja'
-    
-    return await run_command(cmd)
-
-
-async def build_cmake_project(path: str) -> bool:
-    if os.path.exists(os.path.join(path, 'Benchmark.exe')):
-        return
-    build_command = f'cmake --build {path} --config Release --target Benchmark -j 8'
-    return await run_command(build_command)
-
-async def execute_cmake_target(path: str):
-    json_file = os.path.join(path, 'benchmark-iterators.json')
-    if os.path.exists(json_file):
-        return
-    cmd = f'{os.path.join(path, "Benchmark")} --benchmark_format=json --benchmark_out="{json_file}"'
-    return await run_command(cmd)
-
-
-def make_chart(cxx_version: str, path: list[str]):
+def make_chart(cxx_version: str, path: str):
+    print(f'Creating chart for "CXX{cxx_version}" from "{path}/benchmark-iterators.json"')
     with open(os.path.join(path, 'benchmark-iterators.json'), 'r') as benchmarks_file:
         bench_json = json.load(benchmarks_file)
         bench_records = get_benchmark_names_and_time(bench_json)
@@ -120,40 +87,10 @@ def make_chart(cxx_version: str, path: list[str]):
     print(f'{green}Successfully created {filename.format(cxx_version)}')
 
 
-def retry_remove_all(f, path: str, _):
-    try:
-        print(f'Removing "{path}" after retrying...')
-        time.sleep(3)
-        shutil.rmtree(path)
-    except PermissionError:
-        print(f'Cannot remove "{path}" after retrying. Please remove it manually.')
-    f(path)
-
-
-async def make_charts(cxx_versions: list[str], paths: list[str], compiler_path: str):
-    success = await asyncio.gather(*(configure_cmake(cxx_version, path, compiler_path) for cxx_version, path in zip(cxx_versions, paths)))
-    if not any(success):
-        return
-    success = await asyncio.gather(*(build_cmake_project(path) for path in paths))
-    if not any(success):
-        return
-    success = await asyncio.gather(*(execute_cmake_target(path) for path in paths))
-    if not any(success):
-        return
-    success = await asyncio.gather(*(asyncio.to_thread(make_chart, cxx_version, path) for cxx_version, path in zip(cxx_versions, paths)))
-    if not any(success):
-        return
-    try:
-        await asyncio.gather(*(asyncio.to_thread(shutil.rmtree, path, on_error=retry_remove_all) for path in paths))
-    except PermissionError:
-        print('Cannot remove build directory. Please remove it manually.')
-
-
 async def main():
-    cxx_versions = {'11', '14', '17', '20'}
-    compiler_path = sys.argv[1]
-    paths = [os.path.join(os.getcwd(), f'build-{cxx_version}') for cxx_version in cxx_versions]
-    await make_charts(cxx_versions, paths, compiler_path)
+    cxx_version = sys.argv[1]
+    make_chart(cxx_version, os.path.join(os.getcwd(), 'build'))
+    
 
 
 if __name__ == '__main__':
