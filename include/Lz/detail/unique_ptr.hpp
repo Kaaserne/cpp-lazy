cc12fc10266c1d1ced8c704dadf7854412f6c9ac
MarcDirven <github@mdy.mozmail.com>
2025-03-25 23:09:07 +0100

    removed constexpr

--- include/Lz/detail/unique_ptr.hpp
+++ include/Lz/detail/unique_ptr.hpp
@@ -102,7 +102,7 @@ public:
 };
 
 template<class T, class... Args>
-constexpr unique_ptr<T> make_unique(Args&&... args) {
+unique_ptr<T> make_unique(Args&&... args) {
     return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
