8f5fa06f22e796f59129c387685b21c3a482a85f
MarcDirven <github@mdy.mozmail.com>
2025-06-01 14:46:12 +0200

    Add excplicit includes

--- tests/c_string/c_string_forward_decl.cpp
+++ tests/c_string/c_string_forward_decl.cpp
@@ -1,4 +1,8 @@
-#include <c_string_forward_decl.hpp>
+#include "c_string_forward_decl.hpp"
+
+#include <Lz/detail/adaptors/c_string.hpp>
+#include <Lz/detail/iterables/c_string.hpp>
+#include <Lz/detail/iterators/c_string.hpp>
 
 template class lz::detail::c_string_iterable<const char>;
 template class lz::detail::c_string_iterator<const char>;
