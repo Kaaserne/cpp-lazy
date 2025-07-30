f17af6ab8aed99c2a5eec5c5e7711fc0804a5484
Kaaserne <31624073+Kaaserne@users.noreply.github.com>
2025-07-30 17:32:14 +0200

    Fix string output

--- tests/module_tests/module_test.cpp
+++ tests/module_tests/module_test.cpp
@@ -8,7 +8,7 @@ int main() {
     std::vector<int> vec = { 1, 2, 3, 4, 5 };
     auto doubled = vec | lz::map([](int i) { return i * 2; });
     std::cout << doubled << std::endl; // Output: 2, 4, 6, 8, 10
-    std::cout << "Size= = " << lz::size(doubled) << std::endl; // Output: Size= 5
+    std::cout << "Size = " << lz::size(doubled) << std::endl; // Output: Size= 5
 
     for (const auto& val : doubled) {
         std::cout << val << " "; // Output: 2 4 6 8 10
