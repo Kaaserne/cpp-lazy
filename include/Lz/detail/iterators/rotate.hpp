260b431673581725f62dbb1d2004bdf14cb5a6d2
Kaaserne <31624073+Kaaserne@users.noreply.github.com>
2025-10-16 21:53:17 +0200

    Fix one warning for gcc

--- include/Lz/detail/iterators/rotate.hpp
+++ include/Lz/detail/iterators/rotate.hpp
@@ -130,7 +130,7 @@ public:
 
     LZ_CONSTEXPR_CXX_14 difference_type difference(const rotate_iterator& other) const {
         LZ_ASSERT_COMPATIBLE(_iterable.begin() == other._iterable.begin() && _iterable.end() == other._iterable.end());
-        return static_cast<difference_type>(_offset) - static_cast<difference_type>(other._offset);
+        return static_cast<difference_type>(_offset - other._offset);
     }
 
     constexpr difference_type difference(const iter&) const {
