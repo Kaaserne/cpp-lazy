3806ed36abf5a9b034f6c590a089042182b089a1
Kaaserne <31624073+Kaaserne@users.noreply.github.com>
2025-12-18 23:04:29 +0100

    Remove fmtPICFix and add cmake to build essentials zip

--- cmake/fmtPICFix.cmake
+++
@@ -1,5 +0,0 @@
-# This file should be use with the CMAKE_PROJECT_FMT_INCLUDE_BEFORE cache
-# variable to add the -fPIC flag to {fmt}'s directory scope.
-#
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
