fe242d151b2da2392e60327a3c2e132ecc044829
Kaaserne <31624073+Kaaserne@users.noreply.github.com>
2026-02-04 21:06:37 +0100

    Surpress warning for apple zero as null constant

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,3 +1,12 @@
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+#endif
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <doctest/doctest.h>
+
+#if defined(__APPLE__) && defined(__clang__)
+#pragma clang diagnostic pop
+#endif
